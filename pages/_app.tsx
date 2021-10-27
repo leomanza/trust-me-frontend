ca51a0a1fb6b0a4c256685ae6ea52131b21ac6d5
Manzanal, Leandro <lmanzanal@aclaratech.com>
2021-10-26 23:48:01 -0300

    fix build errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/layout";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
